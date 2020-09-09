479b3525fcf4b33d1cfc125e81ab9c6d07395b2f
beeena <mubina.ghazi@gmail.com>
2020-09-09 06:17:24 +0500

    remove extra code

--- packages/react-shared-components/src/components/Layout/components/SubMenu3/SiderMenu.tsx
+++ packages/react-shared-components/src/components/Layout/components/SubMenu3/SiderMenu.tsx
@@ -9,16 +9,6 @@ import { WithFalse } from '../typings';
 import BaseMenu, { BaseMenuProps } from './BaseMenu';
 import MenuCounter from './Counter';
 
-// import React, { useState } from 'react';
-// import * as H from "history";
-// import { Layout } from "antd";
-// import { MenuDataItem, WithFalse } from "./typings";
-// import BaseMenu, { BaseMenuProps } from "../SubMenu3/BaseMenu";
-// import {
-//   MenuUnfoldOutlined,
-//   MenuFoldOutlined
-// } from "@ant-design/icons";
-
 const { Sider } = Layout;
 
 
@@ -89,83 +79,6 @@ export interface SiderMenuProps
 
 export const defaultRenderCollapsedButton = (collapsed?: boolean) =>
   collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />;
-
-// export const defaultRenderLogo = (logo: React.ReactNode): React.ReactNode => {
-//   if (typeof logo === 'string') {
-//     return <img src={logo} alt="logo" />;
-//   }
-//   if (typeof logo === 'function') {
-//     return logo();
-//   }
-//   return logo;
-// };
-
-// export const defaultRenderLogoAndTitle = (
-//   props: SiderMenuProps,
-//   renderKey: string = 'menuHeaderRender',
-// ): React.ReactNode => {
-//   const {
-//     logo = 'https://gw.alipayobjects.com/zos/antfincdn/PmY%24TNNDBI/logo.svg',
-//     title,
-//     layout,
-//   } = props;
-//   const renderFunction = props[renderKey || ''];
-//   if (renderFunction === false) {
-//     return null;
-//   }
-//   const logoDom = defaultRenderLogo(logo);
-//   const titleDom = <h1>{title}</h1>;
-
-//   if (renderFunction) {
-//     // when collapsed, no render title
-//     return renderFunction(logoDom, props.collapsed ? null : titleDom, props);
-//   }
-
-//   if (layout === 'mix' && renderKey === 'menuHeaderRender') {
-//     return null;
-//   }
-
-//   return (
-//     <a>
-//       {logoDom}
-//       {props.collapsed ? null : titleDom}
-//     </a>
-//   );
-// };
-
-// export interface SiderMenuProps {
-//   logo?: React.ReactNode;
-//   onMenuHeaderClick?: (e: React.MouseEvent<HTMLDivElement>) => void;
-//   menuExtraRender?: WithFalse<(props: SiderMenuProps) => React.ReactNode>;
-//   hide?: boolean;
-//   title?: string; 
-//   collapsed?: boolean; 
-//   className?: string;
-//   links?: React.ReactNode[];
-//   layout?: any;
-// }
-
-// export namespace ISiderMenu {
-//   export interface CompProps {
-//     menuExtraRender?: any;
-//     menuData?: MenuDataItem[];
-//     segments:any;
-//     title?: string;
-//     collapsed: boolean;
-//   }
-
-//   export interface StateProps {
-//     location?: H.Location;
-//   }
-
-//   export interface CompState {
-//     collapsed?: boolean;
-//   }
-
-//   export type Props = CompProps & StateProps;
-//   export type State = CompState;
-// }
-
 
 const SiderMenu: React.FC<SiderMenuProps> = (props) => {
   const {
@@ -292,58 +205,6 @@ const SiderMenu: React.FC<SiderMenuProps> = (props) => {
     </>
   );
 };
-
-// export const SiderMenu: React.FC<ISiderMenu.Props> = (props) => {
-
-//   const { menuExtraRender = false } = props;
-//   const [collapsed, setCollapsed]: any = useState(false);
-
-//   const headerDom = defaultRenderLogoAndTitle(props);
-
-//   const extraDom = menuExtraRender && menuExtraRender(props);
-
-//   const {css} = useFela(props);
-
-//   const toggle = () => {
-//     setCollapsed(!collapsed);
-//   };
-//   return (
-//     <Sider
-//         trigger={null}
-//         collapsible
-//         collapsed={collapsed}
-//         breakpoint="lg"
-//         width={256}
-//         collapsedWidth={65}
-//         className={"SiderMenu"} 
-//       >
-//         {headerDom && collapsed && (
-//           <div className={css(styles.antProSiderTitleHide)} id="logo">
-//             {headerDom}
-//           </div>
-//         )}
-//         {headerDom && !collapsed && (
-//           <div className={css(styles.antProSiderLogo)} id="logo">
-//             {headerDom}
-//           </div>
-//         )}
-//         {extraDom && (
-//           <div
-//           className={`css(styles.antProSiderLogo) ${!headerDom && css(styles.antProSiderLogo)}`}
-//           >
-//             {extraDom}
-//           </div>
-//         )}
-//         <BaseMenu {...props } />
-//           <div className={css(styles.btnNavigation)}>
-//             {React.createElement(
-//               collapsed ? MenuUnfoldOutlined : MenuFoldOutlined,
-//               { className: "trigger", onClick: toggle }
-//             )}
-//           </div>
-//       </Sider>
-//   );
-// }
 
 export default SiderMenu;
 
