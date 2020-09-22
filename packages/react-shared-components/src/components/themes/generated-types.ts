6af95a92e481855f9822f2738899544339f3279b
veeramarni <marni.srikanth@gmail.com>
2020-09-22 07:24:28 -0400

    add theme component

---
+++ packages/react-shared-components/src/components/themes/generated-types.ts
@@ -0,0 +1,39 @@
+
+// types generated based on `.json` files from
+// https://jvilk.com/MakeTypes/
+export interface IThemeSettings {
+    type: string;
+    colors: Colors;
+    tokenColors?: (TokenColorsEntity)[] | null;
+}
+export interface Colors {
+    list: {
+        dropBackground: string;
+    };
+    editor: {
+        background: string;
+        foreground: string;
+        inactiveSelectionBackground: string;
+        selectionHighlightBackground: string;
+    };
+    editorIndentGuide: {
+        background: string;
+    };
+    activityBarBadge: {
+        background: string;
+    };
+    sideBarTitle: {
+        foreground: string;
+    };
+}
+export interface TokenColorsEntity {
+    settings: Settings;
+    scope?: string | (string)[] | null;
+    name?: string | null;
+}
+export interface Settings {
+    foreground?: string | null;
+    background?: string | null;
+    fontStyle?: string | null;
+}
+
