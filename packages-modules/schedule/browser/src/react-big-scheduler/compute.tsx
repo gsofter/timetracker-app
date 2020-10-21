55b76103460ea838a9043291c7fe4fb702069c03
beeena <mubina.ghazi@gmail.com>
2020-10-21 16:28:50 +0500

    added demos

--- packages-modules/schedule/browser/src/react-big-scheduler/compute.tsx
+++ packages-modules/schedule/browser/src/react-big-scheduler/compute.tsx
@@ -1,24 +1,39 @@
 import * as React from 'react';
 import { IMenuPosition } from '@common-stack/client-react';
 import { getFilteredMenus, getFilteredRoutes } from '../utils';
-import { Home } from './components/Home';
+import { Home, Demo1, Demo2 } from './components/index';
 import { ScheduleOutlined } from '@ant-design/icons';
 
 
 
 export const schedulerPage: any[] = [
     {
-        component: Home,
-        tab: 'React Big Scheduler',
+        exact: false,
+        icon: <ScheduleOutlined />,
         key: 'scheduler',
-        position: IMenuPosition.BOTTOM,
+        component: Demo2,
+        tab: 'React Big Scheduler',
+        position: IMenuPosition.MIDDLE,
         name: 'React Big Scheduler',
         path: '/react-big-scheduler',
-        icon: <ScheduleOutlined />
+    },
+    {
+        key: 'demo',
+        name: 'Demo 1',
+        component: Demo1,
+        position: IMenuPosition.MIDDLE,
+        path: '/react-big-scheduler/demo',
+    },
+    {
+        key: 'demo2',
+        name: 'Demo 2',
+        component: Demo2,
+        position: IMenuPosition.MIDDLE,
+        path: '/react-big-scheduler/demo2',
     },
 ];
 
-const selectedRoutesAndMenus = ['scheduler'];
+const selectedRoutesAndMenus = ['scheduler','demo', 'demo2'];
 
 // get menus
 const filteredMenus = getFilteredMenus(schedulerPage, selectedRoutesAndMenus);
