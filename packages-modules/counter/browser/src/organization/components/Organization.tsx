45967ecde49567235cef5c284376403e97af8969
Madhav <madhavdev2009@gmail.com>
2020-11-20 20:50:19 +0530

    Top Menus and OrgName Integration

--- packages-modules/counter/browser/src/organization/components/Organization.tsx
+++ packages-modules/counter/browser/src/organization/components/Organization.tsx
@@ -3,10 +3,6 @@ import * as React from 'react';
 export const Organization = () => (
     <div>
         <h1>Organizations Menu</h1>
-        <select>
-            <option>sample</option>
-            <option>sample1</option>
-            <option>sample2</option>
-        </select>
+        <button>Generate Organizations</button>
     </div>
 );
