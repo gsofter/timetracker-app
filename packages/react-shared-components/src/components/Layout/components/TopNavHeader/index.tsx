f522f238c3eab1594d64d6aa8b454a80a209732f
Veera Marni <marni.srikanth@gmail.com>
2020-11-22 06:24:11 -0500

    correct tslint

--- packages/react-shared-components/src/components/Layout/components/TopNavHeader/index.tsx
+++ packages/react-shared-components/src/components/Layout/components/TopNavHeader/index.tsx
@@ -1,21 +1,21 @@
-import React, { useRef, useState } from "react";
-import classNames from "classnames";
-import ResizeObserver from "rc-resize-observer";
-import { useFela } from "react-fela";
-import AvatarDropdown from "../GlobalHeader/AvatarDropdown";
+import React, { useRef, useState } from 'react';
+import classNames from 'classnames';
+import ResizeObserver from 'rc-resize-observer';
+import { useFela } from 'react-fela';
+import AvatarDropdown from '../GlobalHeader/AvatarDropdown';
 
 import {
   SiderMenuProps,
   defaultRenderLogoAndTitle,
-} from "../SubMenu3/SiderMenu";
+} from '../SubMenu3/SiderMenu';
 
-import BaseMenu from "../SubMenu3/BaseMenu";
-import { HeaderViewProps } from "../Header";
+import BaseMenu from '../SubMenu3/BaseMenu';
+import { HeaderViewProps } from '../Header';
 
 export type TopNavHeaderProps = SiderMenuProps & {
   logo?: React.ReactNode;
   onCollapse?: (collapse: boolean) => void;
-  rightContentRender?: HeaderViewProps["rightContentRender"];
+  rightContentRender?: HeaderViewProps['rightContentRender'];
 };
 
 /**
@@ -26,7 +26,7 @@ const RightContent: React.FC<TopNavHeaderProps> = ({
   rightContentRender,
   ...props
 }) => {
-  const [rightSize, setRightSize] = useState<number | string>("auto");
+  const [rightSize, setRightSize] = useState<number | string>('auto');
   const { css } = useFela(props);
 
   return (
@@ -63,18 +63,18 @@ const TopNavHeader: React.FC<TopNavHeaderProps> = (props) => {
     className: propsClassName,
     style,
     layout,
-    primaryColor
+    primaryColor,
   } = props;
 
   
-  const baseClassName = "ant-pro-top-nav-header";
+  const baseClassName = 'ant-pro-top-nav-header';
   const headerDom = defaultRenderLogoAndTitle(
     { ...props, collapsed: false },
-    layout === "mix" ? "headerTitleRender" : undefined
+    layout === 'mix' ? 'headerTitleRender' : undefined,
   );
 
   const className = classNames(baseClassName, propsClassName, {
-    light: theme === "light",
+    light: theme === 'light',
   });
 
   const { css } = useFela({...props, primaryColor});
@@ -84,7 +84,7 @@ const TopNavHeader: React.FC<TopNavHeaderProps> = (props) => {
         <div
           ref={ref}
           className={`${baseClassName}-main ${
-            contentWidth === "Fixed" ? "wide" : ""
+            contentWidth === 'Fixed' ? 'wide' : ''
           }`}
         >
           {headerDom && (
@@ -92,7 +92,7 @@ const TopNavHeader: React.FC<TopNavHeaderProps> = (props) => {
               className={`${baseClassName}-main-left`}
               onClick={onMenuHeaderClick}
             >
-              <div className={`${baseClassName}-logo`} key="logo" id="logo">
+              <div className={`${baseClassName}-logo`} key='logo' id='logo'>
                 {headerDom}
               </div>
             </div>
@@ -114,71 +114,71 @@ export default TopNavHeader;
 
 const styleSheet: any = {
   topHeaderStyle: ({theme, primaryColor}) => ({
-    "& .ant-pro-top-nav-header": {
-      position: "relative",
-      width: "100%",
-      height: "100%",
-      boxShadow: "0 1px 4px 0 rgba(0,21,41,0.12)",
-      transition: "background 0.3s, width 0.2s",
+    '& .ant-pro-top-nav-header': {
+      position: 'relative',
+      width: '100%',
+      height: '100%',
+      boxShadow: '0 1px 4px 0 rgba(0,21,41,0.12)',
+      transition: 'background 0.3s, width 0.2s',
     },
-    "& .ant-menu-dark.ant-menu-horizontal > .ant-menu-item:hover, .ant-menu.ant-menu-dark .ant-menu-item-selected": {
+    '& .ant-menu-dark.ant-menu-horizontal > .ant-menu-item:hover, .ant-menu.ant-menu-dark .ant-menu-item-selected': {
       background: primaryColor ? primaryColor : '#1890ff'
     },
-    "& .ant-pro-top-nav-header .ant-menu-submenu.ant-menu-submenu-horizontal": {
-      height: "100%",
+    '& .ant-pro-top-nav-header .ant-menu-submenu.ant-menu-submenu-horizontal': {
+      height: '100%',
     },
-    "& .ant-pro-top-nav-header .ant-menu-submenu.ant-menu-submenu-horizontal .ant-menu-submenu-title": {
-      height: "100%",
+    '& .ant-pro-top-nav-header .ant-menu-submenu.ant-menu-submenu-horizontal .ant-menu-submenu-title': {
+      height: '100%',
     },
-    "& .ant-pro-top-nav-header.light": {
-      backgroundColor: "#fff",
+    '& .ant-pro-top-nav-header.light': {
+      backgroundColor: '#fff',
     },
-    "& .ant-menu-submenu .ant-menu-submenu-horizontal .ant-menu-submenu-selected": {
+    '& .ant-menu-submenu .ant-menu-submenu-horizontal .ant-menu-submenu-selected': {
       backgroundColor: primaryColor ? primaryColor : '#1890ff'
     },
-    "& .ant-menu-submenu .ant-menu-submenu-horizontal .ant-menu-submenu-open .ant-menu-submenu-active, .ant-menu-item:hover, .ant-menu-item-active, .ant-menu:not(.ant-menu-inline) .ant-menu-submenu-open, .ant-menu-submenu-active, .ant-menu-submenu-title:hover, .ant-menu-submenu-selected, .ant-menu-horizontal:not(.ant-menu-dark) > .ant-menu-item:hover, ant-menu-item a:hover, .ant-menu-horizontal:not(.ant-menu-dark) > .ant-menu-submenu:hover, .ant-menu-light .ant-menu-item:hover > a, .ant-menu-light .ant-menu-item-active > a, .ant-menu-horizontal:not(.ant-menu-dark) > .ant-menu-item-selected a, .ant-menu-item-selected a, .ant-menu-horizontal:not(.ant-menu-dark) > .ant-menu-item a:hover, .ant-menu-horizontal:not(.ant-menu-dark) > .ant-menu-submenu-selected": {
+    '& .ant-menu-submenu .ant-menu-submenu-horizontal .ant-menu-submenu-open .ant-menu-submenu-active, .ant-menu-item:hover, .ant-menu-item-active, .ant-menu:not(.ant-menu-inline) .ant-menu-submenu-open, .ant-menu-submenu-active, .ant-menu-submenu-title:hover, .ant-menu-submenu-selected, .ant-menu-horizontal:not(.ant-menu-dark) > .ant-menu-item:hover, ant-menu-item a:hover, .ant-menu-horizontal:not(.ant-menu-dark) > .ant-menu-submenu:hover, .ant-menu-light .ant-menu-item:hover > a, .ant-menu-light .ant-menu-item-active > a, .ant-menu-horizontal:not(.ant-menu-dark) > .ant-menu-item-selected a, .ant-menu-item-selected a, .ant-menu-horizontal:not(.ant-menu-dark) > .ant-menu-item a:hover, .ant-menu-horizontal:not(.ant-menu-dark) > .ant-menu-submenu-selected': {
       color: primaryColor ? primaryColor : '#1890ff'
     },
-    "& .ant-pro-top-nav-header.light .anticon": {
+    '& .ant-pro-top-nav-header.light .anticon': {
       color: 'inherit',
     },
-    "& .ant-pro-top-nav-header-main": {
-      display: "flex",
-      height: "100%",
-      paddingLeft: "16px",
+    '& .ant-pro-top-nav-header-main': {
+      display: 'flex',
+      height: '100%',
+      paddingLeft: '16px',
     },
-    "& .ant-pro-top-nav-header-main-left": {
-      display: "flex",
-      minWidth: "192px",
+    '& .ant-pro-top-nav-header-main-left': {
+      display: 'flex',
+      minWidth: '192px',
     },
-    "& .ant-pro-top-nav-header .anticon": {
+    '& .ant-pro-top-nav-header .anticon': {
       color: 'inherit',
     },
-    "& .ant-pro-top-nav-header-logo": {
-      position: "relative",
-      minWidth: "165px",
-      height: "100%",
-      overflow: "hidden",
-      transition: "all 0.3s",
+    '& .ant-pro-top-nav-header-logo': {
+      position: 'relative',
+      minWidth: '165px',
+      height: '100%',
+      overflow: 'hidden',
+      transition: 'all 0.3s',
     },
-    "& .ant-pro-top-nav-header-logo img": {
-      display: "inline-block",
-      height: "32px",
-      verticalAlign: "middle",
+    '& .ant-pro-top-nav-header-logo img': {
+      display: 'inline-block',
+      height: '32px',
+      verticalAlign: 'middle',
     },
-    "& .ant-pro-top-nav-header-logo h1": {
-      display: "inline-block",
-      margin: "0 0 0 12px",
-      color: theme === "light" ? primaryColor : '#fff',
-      fontWeight: "400",
-      fontSize: "16px",
+    '& .ant-pro-top-nav-header-logo h1': {
+      display: 'inline-block',
+      margin: '0 0 0 12px',
+      color: theme === 'light' ? primaryColor : '#fff',
+      fontWeight: '400',
+      fontSize: '16px',
     },
-    "& .ant-pro-top-nav-header-menu": {
+    '& .ant-pro-top-nav-header-menu': {
       minWidth: 0,
     },
-    "& .ant-pro-top-nav-header-menu .ant-menu.ant-menu-horizontal": {
-      height: "100%",
-      border: "none",
+    '& .ant-pro-top-nav-header-menu .ant-menu.ant-menu-horizontal': {
+      height: '100%',
+      border: 'none',
     },
   }),
   widthRightSize: (rightSize) => ({
@@ -186,6 +186,6 @@ const styleSheet: any = {
   }),
 
   displayFlex: (props) => ({
-    flex: "1",
+    flex: '1',
   }),
 };
