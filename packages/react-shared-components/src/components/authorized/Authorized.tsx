b1f5fefa51e0c9263c03abcc6b2a6d4e6b7cbd8e
Veera Marni <marni.srikanth@gmail.com>
2020-12-04 12:16:12 -0500

    update update

--- packages/react-shared-components/src/components/authorized/Authorized.tsx
+++ packages/react-shared-components/src/components/authorized/Authorized.tsx
@@ -1,45 +1,35 @@
-
 import React from 'react';
 import { Result } from 'antd';
+import check, { IAuthorityType } from './CheckPermissions';
 
-
-export type IAuthorityType =
-    | undefined
-    | string
-    | string[]
-    | Promise<boolean>
-    | ((currentAuthority: string | string[]) => IAuthorityType);
+import AuthorizedRoute from './AuthorizedRoute';
+import Secured from './Secured';
 
 interface AuthorizedProps {
-    autority: IAuthorityType;
-    noMatch?: React.ReactNode;
+  authority: IAuthorityType;
+  noMatch?: React.ReactNode;
 }
 
+type IAuthorizedType = React.FunctionComponent<AuthorizedProps> & {
+  Secured: typeof Secured;
+  check: typeof check;
+  AuthorizedRoute: typeof AuthorizedRoute;
+};
 
-/**
- * 通用权限检查方法
- * Common check permissions method
- * @param { 权限判定 | Permission judgment } authority
- * @param { 你的权限 | Your permission description } currentAuthority
- * @param { 通过的组件 | Passing components } target
- * @param { 未通过的组件 | no pass components } Exception
- */
-const checkPermissions = 
-
-const Authorized: React.SFC<AuthorizedProps> = ({
-    children,
-    authority,
-    noMatch = (
-        <Result
-            status="403"
-            title="403"
-            subTitle="Sorry, you are not authorized to access this page."
-        />
-    ),
+const Authorized: React.FunctionComponent<AuthorizedProps> = ({
+  children,
+  authority,
+  noMatch = (
+    <Result
+      status="403"
+      title="403"
+      subTitle="Sorry, you are not authorized to access this page."
+    />
+  ),
 }) => {
-    const childrenRender: React.ReactNode = typeof children === 'undefined' ? null : children;
-    const dom = check(authority, childrenRender, noMatch);
-    return <>(dom)</>;
-}
+  const childrenRender: React.ReactNode = typeof children === 'undefined' ? null : children;
+  const dom = check(authority, childrenRender, noMatch);
+  return <>{dom}</>;
+};
 
-export default Authorized;
+export default Authorized as IAuthorizedType;
