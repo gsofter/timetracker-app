cc0ff4ba01598b89e1d331a00ebe1628db33d5be
gsoftinnovate <guru.softwaremaster@mail.ru>
2020-12-10 11:17:56 -0700

    convert GridContent less

--- packages/components/src/components/Layout/components/GridContent/index.tsx
+++ packages/components/src/components/Layout/components/GridContent/index.tsx
@@ -4,7 +4,8 @@ import { ConfigProvider } from 'antd';
 
 import RouteContext from '../RouteContext';
 import { PureSettings } from '../defaultSettings';
-
+import { Properties } from 'csstype'
+import { useFela } from 'react-fela'
 interface GridContentProps {
   contentWidth?: PureSettings['contentWidth'];
   children: React.ReactNode;
@@ -26,17 +27,31 @@ const GridContent: React.FC<GridContentProps> = (props) => {
   const prefixCls = props.prefixCls || getPrefixCls('pro');
   const contentWidth = propsContentWidth || value.contentWidth;
   let className = `${prefixCls}-grid-content`;
-
+  const { css, theme } = useFela();
   return (
-    <div
-      className={classNames(className, propsClassName, {
-        wide: contentWidth === 'Fixed',
-      })}
-      style={style}
-    >
-      <div className={`${prefixCls}-grid-content-children`}>{children}</div>
+    <div className={css(styleSheet.gridContentStyle)}>
+      <div
+        className={classNames(className, propsClassName, {
+          wide: contentWidth === 'Fixed',
+        })}
+        style={style}
+      >
+        <div className={`${prefixCls}-grid-content-children`}>{children}</div>
+      </div>
     </div>
   );
 };
 
+const styleSheet: { [key: string]: (obj) => Properties } = {
+  gridContentStyle: ({ theme, primaryColor, layout }) => ({
+    display: 'inherit',
+    '& .ant-prefix-pro-grid-content': {
+      width: '100%',
+      '&.wide': {
+        maxWidth: '1200px',
+        margin: '0 auto',
+      }
+    }
+  })
+}
 export default GridContent;
