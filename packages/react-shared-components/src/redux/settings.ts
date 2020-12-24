a1fc7fbe6d89313302528e46aeb4d2628fb43073
Veera Marni <marni.srikanth@gmail.com>
2020-12-24 17:56:06 -0500

    fixes ssr break

--- packages/react-shared-components/src/redux/settings.ts
+++ packages/react-shared-components/src/redux/settings.ts
@@ -10,20 +10,25 @@ export interface SettingModelType {
 }
 
 const updateColorWeak: (colorWeak: boolean) => void = (colorWeak) => {
-    const root = document.getElementById('root');
-    if (root) {
-        root.className = colorWeak ? 'colorWeak' : '';
+    // @sri to avoid breaking during SSR, split into to checks
+    if (typeof window !== 'undefined') {
+        const root = document.getElementById('root');
+        if (root) {
+            root.className = colorWeak ? 'colorWeak' : '';
+        }
     }
 };
 const SettingModel: SettingModelType = {
     namespace: 'settings',
     state: defaultSettings,
     reducers: {
-        settings(state = defaultSettings, { payload = {}}) {
+        settings(state = defaultSettings, { payload = {} }) {
             const { colorWeak, contentWidth } = payload;
-
-            if (state.contentWidth !== contentWidth && window.dispatchEvent) {
-                window.dispatchEvent(new Event('resize'));
+            // @sri to avoid breaking during SSR, split into to checks
+            if (state.contentWidth !== contentWidth) {
+                if (typeof window !== 'undefined') {
+                    window.dispatchEvent(new Event('resize'));
+                }
             }
             updateColorWeak(!!colorWeak);
             return {
