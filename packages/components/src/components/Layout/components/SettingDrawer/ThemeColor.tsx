6fea5a3db43aa348a9468db6b2eabcc2e0113336
Veera Marni <marni.srikanth@gmail.com>
2021-01-05 09:32:20 -0500

    Corrected Theme Color styles

--- packages/components/src/components/Layout/components/SettingDrawer/ThemeColor.tsx
+++ packages/components/src/components/Layout/components/SettingDrawer/ThemeColor.tsx
@@ -5,7 +5,7 @@ import { Tooltip } from "antd";
 import React from "react";
 import { genThemeToString } from "../utils/utils";
 import { useFela } from "react-fela";
-import { Properties } from 'csstype'
+import { styleSheet } from './ThemeColorStyles';
 export interface TagProps {
   color: string;
   check: boolean;
@@ -14,15 +14,13 @@ export interface TagProps {
   onClick?: () => void;
 }
 
-const Tag: React.FC<TagProps> = React.forwardRef(
-  ({ color, check, ...rest }, ref) => (
-    <div {...rest} style={{ backgroundColor: color }} ref={ref as any}>
-      {check ? <CheckOutlined /> : ""}
-    </div>
-  )
-);
+const Tag: React.FC<TagProps> = React.forwardRef(({ color, check, ...rest }, ref) => (
+  <div {...rest} style={{ backgroundColor: color }} ref={ref as any}>
+    {check ? <CheckOutlined /> : ''}
+  </div>
+));
 
-export interface ThemeColorProps {
+export type ThemeColorProps = {
   colors?: {
     key: string;
     color: string;
@@ -30,20 +28,20 @@ export interface ThemeColorProps {
   value: string;
   onChange: (color: string) => void;
   formatMessage: (data: { id: any; defaultMessage?: string }) => string;
-}
+};
 
-const ThemeColor: React.ForwardRefRenderFunction<
-  HTMLDivElement,
-  ThemeColorProps
-> = ({ colors, value, onChange, formatMessage }, ref) => {
+const ThemeColor: React.ForwardRefRenderFunction<HTMLDivElement, ThemeColorProps> = (
+  { colors, value, onChange, formatMessage },
+  ref,
+) => {
   const colorList = colors || [];
   if (colorList.length < 1) {
     return null;
   }
   const { css } = useFela();
   return (
-    <div className={css(styleSheet.themecolor)}>
-      <div ref={ref} className="theme-color">
+    <div className={css(styleSheet.settingDrawerContent as any)}>
+      <div className="theme-color" ref={ref}>
         <div className="theme-color-content">
           {colorList.map(({ key, color }) => {
             const themeKey = genThemeToString(color) || key;
@@ -61,8 +59,8 @@ const ThemeColor: React.ForwardRefRenderFunction<
                 <Tag
                   className="theme-color-block"
                   color={color}
-                  check={value === color || genThemeToString(value) === color}
-                  onClick={() => onChange && onChange(color)}
+                  check={value === key || genThemeToString(value) === key}
+                  onClick={() => onChange && onChange(key)}
                 />
               </Tooltip>
             );
@@ -73,31 +71,4 @@ const ThemeColor: React.ForwardRefRenderFunction<
   );
 };
 
-const styleSheet: { [key: string]: (obj) => Properties } = {
-
-  themecolor: ({ theme, primaryColor, layout }) => ({
-    display: 'inherit',
-    '& .ant-pro-setting-drawer-content .theme-color': {
-      marginTop: '16px',
-      overflow: 'hidden',
-    },
-    '& .ant-pro-setting-drawer-content .theme-color .theme-color-title': {
-      marginBottom: '12px',
-      fontSize: '14px',
-      lineHeight: '22px',
-    },
-    '& .ant-pro-setting-drawer-content .theme-color .theme-color-block': {
-      float: 'left',
-      width: '20px',
-      height: '20px',
-      marginRight: '8px',
-      marginTop: '8px',
-      color: '#fff',
-      fontWeight: 'bold',
-      textAlign: 'center',
-      borderRadius: '2px',
-      cursor: 'pointer'
-    }
-  })
-}
 export default React.forwardRef(ThemeColor);
