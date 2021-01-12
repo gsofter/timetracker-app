9c493595e6b6f42b4281d23be319c8907511c30c
gsofter <real.kingstar@outlook.com>
2021-01-12 07:34:31 -0500

    add loading to submit

--- packages-modules/schedule/browser/src/react-big-scheduler/components/Timesheet/Timesheet.tsx
+++ packages-modules/schedule/browser/src/react-big-scheduler/components/Timesheet/Timesheet.tsx
@@ -57,32 +57,6 @@ interface ITimesheetProps {
   handleChangeUser: (any) => void;
 }
 
-class CalendarEvent {
-  title: string;
-  allDay: boolean;
-  start: Date;
-  end: Date;
-  desc: string;
-  resourceId?: string;
-  tooltip?: string;
-
-  constructor(
-    _title: string,
-    _start: Date,
-    _endDate: Date,
-    _allDay?: boolean,
-    _desc?: string,
-    _resourceId?: string,
-  ) {
-    this.title = _title;
-    this.allDay = _allDay || false;
-    this.start = _start;
-    this.end = _endDate;
-    this.desc = _desc || '';
-    this.resourceId = _resourceId;
-  }
-}
-
 function SelectableCalendar({
   events,
   handleAddTimesheetEvent,
@@ -250,7 +224,7 @@ function SelectableCalendar({
               Reset
             </Button>
             &nbsp;
-            <Button type="primary" htmlType="submit">
+            <Button type="primary" htmlType="submit" loading={loading}>
               Submit
             </Button>
             &nbsp;
