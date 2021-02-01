18a2d460a1959431bdb76ac93c37f58b5f5e6fe7
gsofter <real.kingstar@outlook.com>
2021-01-31 19:00:51 -0500

    fix schema and resolver mismatching

--- packages-modules/timetracker/server/src/resolvers/timesheet-resolver.ts
+++ packages-modules/timetracker/server/src/resolvers/timesheet-resolver.ts
@@ -1,21 +1,21 @@
 import { DEFAULT_USER } from '../constants'
 export const resolver = (options) => ({
     Query: {
-      getTimeSheets: (root, args, { timeSheetService }) => {
+      getTimesheets: (root, args, { timeSheetService }) => {
         options.logger.trace('(Query.getTimeSheets) args %j', args)
         return timeSheetService.getTimeSheetEvents(DEFAULT_USER);
       },
     },
     Mutation: {
-      addTimeSheet: (root, args, { timeSheetService }) => {
+      createTimesheet: (root, args, { timeSheetService }) => {
         options.logger.trace('(Mutation.addTimeSheet) args %j', args)
         return timeSheetService.createTimeSheet(DEFAULT_USER, args.request);
       },
-      updateTimeSheet: (root, args, { timeSheetService }) => {
+      updateTimesheet: (root, args, { timeSheetService }) => {
         options.logger.trace('(Mutation.updateTimeSheet) args %j', args)
         return timeSheetService.updateTimeSheet(DEFAULT_USER, args.eventId, args.request);
       },
-      removeTimeSheet: (root, args, { timeSheetService }) => {
+      removeTimesheet: (root, args, { timeSheetService }) => {
         options.logger.trace('(Mutation.removeTimeSheet) args %j', args)
         return timeSheetService.removeTimeSheet(DEFAULT_USER, args.eventId);
       } 
