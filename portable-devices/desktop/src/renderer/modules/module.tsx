d0a9557f3412e5bda95a66d92fee90e00ddc422b
Veera Marni <marni.srikanth@gmail.com>
2021-03-02 16:54:28 -0500

    update changes

--- portable-devices/desktop/src/renderer/modules/module.tsx
+++ portable-devices/desktop/src/renderer/modules/module.tsx
@@ -1,43 +1,40 @@
 import * as React from 'react';
-import { Layout } from 'antd';
-import { Route, Switch } from 'react-router';
-import { Feature, FeatureWithRouterFactory } from '@common-stack/client-react';
+import { Feature, FeatureWithRouterFactory, renderRoutes2 } from '@common-stack/client-react';
+import { ThemeProvider } from '@admin-layout/components';
 import LayoutModule, { ProLayout } from '@admin-layout/react-shared-components';
 import CounterModule from '@admin-layout/counter-module-browser';
-import PlatformModule from '@admin-layout/platform-browser';
+import PlatformModule from '@adminide-stack/platform-browser';
 import ScheduleModule from '@admin-layout/schedule-module-browser';
 import ActivityModule from '@admin-layout/activity-module-browser';
 import TimeTrackerModule from '@admin-layout/timetracker-module-browser';
-import { SiderMenu } from './layout';
+import AccountsModule from "@adminide-stack/account-api-browser";
+import AuthModule from "@adminide-stack/user-auth0-browser";
+import ExtensionModule from '@adminide-stack/extension-module-browser';
 
 const features = new Feature(
-    FeatureWithRouterFactory,
-    LayoutModule,
-    PlatformModule,
-    CounterModule,
-    ScheduleModule,
-    TimeTrackerModule,
-    ActivityModule,
+  FeatureWithRouterFactory,
+  LayoutModule,
+  AuthModule,
+  AccountsModule,
+  CounterModule,
+  ScheduleModule,
+  TimeTrackerModule,
+  ActivityModule,
+  PlatformModule,
+  ExtensionModule,
 );
 
-console.log(features.getMenus());
+const configuredRoutes =  features.getConfiguredRoutes();
 
-export const MainRoute = props => (
-    <Layout hasSider={true} style={{ minHeight: '100vh', display: 'flex' }}>
-        <SiderMenu
-            collapsed={false}
-            menuData={features.getMenus()}
-            location={window.location as any}
-            segments={features.sidebarSegments}
-        />
-        <Layout>
-            <Layout.Content style={{ height: '100%' }}>
-                <section className="flex-grow" style={{ height: '100%' }}>
-                    {features.getRoutes()}
-                </section>
-            </Layout.Content>
-        </Layout>
-    </Layout>
-);
-
+const routes = renderRoutes2({ routes: configuredRoutes});
+// console.log(sharedModule);
+export const MainRoute = props => {
+  return (
+    <React.Suspense fallback={<span>Loading....</span>}>
+      <ThemeProvider>
+        {routes}
+      </ThemeProvider>
+    </React.Suspense>
+  );
+};
 export default features;
--- portable-devices/desktop/webpack.main.additions.js
+++ portable-devices/desktop/webpack.main.additions.js
@@ -1,7 +1,34 @@
+const path = require('path');
+var nodeExternals = require('webpack-node-externals');
+const debug = process.env.DEBUGGING || false;
+const {merge} = require('webpack-merge');
+const webpack = require('webpack');
+const Dotenv = require('dotenv-webpack');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const LodashModuleReplacementPlugin = require('lodash-webpack-plugin');
 
 
 let config = {
     target: 'electron-main',
+    entry: './src/main/app.ts',
+    output: {
+        filename: 'index.js',
+    },
+    plugins: [
+        new CopyWebpackPlugin([{
+            from: '../../tools/esm-wrapper.js',
+            to: 'index.js',
+        }]),
+        new LodashModuleReplacementPlugin({
+            // Necessary as a workaround for https://github.com/apollographql/react-apollo/issues/1831
+            flattening: true,
+            shorthands: true
+          }),
+    ],
+    externals: [
+        nodeExternals(),
+        nodeExternals({ allowlist: [/webpack\/hot/i, /babel-polyfill/], modulesDir: "../../node_modules" })
+    ],
 }
 
 
