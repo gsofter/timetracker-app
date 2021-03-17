a8c76fa3bc51bdec6cb49274bec304205258204a
Veera Marni <marni.srikanth@gmail.com>
2021-03-17 10:59:22 -0400

    update develop

--- packages-modules/timetracker/browser/src/timetracker/compute.tsx
+++ packages-modules/timetracker/browser/src/timetracker/compute.tsx
@@ -17,8 +17,7 @@ import {
 const Home = React.lazy(() => import('./containers/Home'));
 const TimeTracker = React.lazy(() => import('./containers/MainPage'));
 const Timesheet = React.lazy(() => import('./containers/TimesheetPage'));
-const ProjectsPage = React.lazy(() => import('./containers/ProjectsPage'));
-const ClientsPage = React.lazy(() => import('./containers/ClientsPage'));
+
 export const timePageStore: any[] = [
   {
     exact: false,
@@ -48,54 +47,37 @@ export const timePageStore: any[] = [
     position: IMenuPosition.MIDDLE,
     path: '/:orgName/time-tracker/timesheet',
     priority: 3,
-  },
-  {
-    exact: true,
-    key: 'timeTracker.projects',
-    name: 'Projects',
-    component: ProjectsPage,
-    position: IMenuPosition.MIDDLE,
-    path: '/:orgName/time-tracker/projects',
-    priority: 4,
-  },
-  {
-    exact: true,
-    key: 'timeTracker.clients',
-    name: 'Clients',
-    component: ClientsPage,
-    position: IMenuPosition.MIDDLE,
-    path: '/:orgName/time-tracker/clients',
-    priority: 5,
-  },{
-    name: 'Settings',
-    key: 'timetracker-settings',
-    path: '/:orgName/time-tracker/settings',
-    hideInMenu: true,
-    tab: 'Settings',
-    authority: [IPreDefineAccountPermissions.manageTeams],
-    component: (props) => {
-        const { orgName, teamName } = useParams() as any;
-        const generatedTeamUri = generateResourceUri(
-            IConfigCollectionName.teams,
-            { name: teamName, orgName: orgName },
-            'settings',
-        );
-        return (
-            <WithConfigurationEnhanced
-                settingsUri={generatedTeamUri}
-                permissionKeys={[IPreDefineAccountPermissions.manageTeams]}
-                configKey="organization.teams.visibility"
-                resourceName="Teams"
-            >
-                <ResourceSettings
-                    settingsUri={generatedTeamUri}
-                    target={ConfigurationTarget.ORGANIZATION_RESOURCE}
-                    showSidebar={false}
-                />
-            </WithConfigurationEnhanced>
-        );
-    },
-},
+  }, 
+  // {
+  //   name: 'Settings',
+  //   key: 'timetracker-settings',
+  //   path: '/:orgName/time-tracker/settings',
+  //   hideInMenu: true,
+  //   tab: 'Settings',
+  //   authority: [IPreDefineAccountPermissions.manageTeams],
+  //   component: (props) => {
+  //     const { orgName, teamName } = useParams() as any;
+  //     const generatedTeamUri = generateResourceUri(
+  //       IConfigCollectionName.teams,
+  //       { name: teamName, orgName: orgName },
+  //       'settings',
+  //     );
+  //     return (
+  //       <WithConfigurationEnhanced
+  //         settingsUri={generatedTeamUri}
+  //         permissionKeys={[IPreDefineAccountPermissions.manageTeams]}
+  //         configKey="organization.teams.visibility"
+  //         resourceName="Teams"
+  //       >
+  //         <ResourceSettings
+  //           settingsUri={generatedTeamUri}
+  //           target={ConfigurationTarget.ORGANIZATION_RESOURCE}
+  //           showSidebar={false}
+  //         />
+  //       </WithConfigurationEnhanced>
+  //     );
+  //   },
+  // },
 ];
 
 const selectedRoutesAndMenus = [
