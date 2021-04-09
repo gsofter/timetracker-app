844a11fa984206741ccbd966246c1b785fb576d3
Veera Marni <marni.srikanth@gmail.com>
2021-04-09 10:02:00 -0400

    add migration

---
+++ servers/backend-server/src/utils/migrations.ts
@@ -0,0 +1,33 @@
+import { Schema, Connection } from 'mongoose';
+import { Container} from 'inversify';
+import { IDatabaseMigration } from '@adminide-stack/core';
+
+export const MigrationSchema = new Schema({
+    migrated_at: Date,
+    name: { required: true, type: String },
+});
+
+export async function migrate(db: Connection, container: Container) {
+    try {
+        const migrations  = container.getAll<IDatabaseMigration>('MongodbMigration');
+        const model = db.model<any, any>('Migration', MigrationSchema);
+        return Promise.all(
+            migrations.map(async (migration) => {
+                const exists = await model.findOne({ name: migration.id });
+                if (!exists) {
+                    try {
+                        await migration.up();
+                        await model.create({ name: migration.id, migrated_at: new Date() });
+                    } catch (e) {
+                        console.log(`Can not process migration ${migration.id}: `, e);
+                    }
+                }
+    
+                return migration.id;
+            }),
+        );
+    } catch(err){
+        console.warn('ignoring migrate database due to ', err.message);
+    }
+
+}
