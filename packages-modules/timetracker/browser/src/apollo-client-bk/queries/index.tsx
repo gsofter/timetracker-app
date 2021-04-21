bda1d2431aab379e8ea0fc3f192cdaf5896bbaa3
Veera Marni <marni.srikanth@gmail.com>
2021-04-21 13:36:54 -0400

    clean up

--- packages-modules/timetracker/browser/src/apollo-client-bk/queries/index.tsx
+++
@@ -1,4 +0,0 @@
-export const GET_PROJECTS_CLIENT = require('./getProjects.client.gql');
-export const GET_TAGS_CLIENT = require('./getTags.client.gql');
-export const GET_MEMBERS_CLIENT = require('./getMembers.client.gql');
-export const GET_SETTINGS_CLIENT = require('./getSettings.client.gql');
