0a25f5ba500dfd32cdf740bf44220cd79a77e56b
gsofter <real.kingstar@outlook.com>
2021-05-02 04:31:31 -0700

    merge develop

--- servers/backend-server/src/middleware/persistedQuery.ts
+++ servers/backend-server/src/middleware/persistedQuery.ts
@@ -1,40 +1,35 @@
 import { invert, isArray } from 'lodash';
-import { GRAPHIQL_ROUTE } from '../ENDPOINTS';
 import { logger } from '@cdm-logger/server';
+import { GRAPHIQL_ROUTE } from '../ENDPOINTS';
 
-let reqlib: any = require('app-root-path');
+const reqlib: any = require('app-root-path');
 
-let persistCache = true;
+const persistCache = true;
 let queryMap;
 try {
     queryMap = reqlib.require('@admin-layout/platform-browser/extracted_queries.json');
-
 } catch (err) {
     logger.warn('extracted_queries.json file is unavailable, disabling persist queries');
 }
 export const persistedQueryMiddleware = (req, res, next) => {
-
     if (queryMap) {
         const invertedMap = invert(queryMap);
 
         if (isArray(req.body)) {
-            req.body = req.body.map(body => {
-                const id = body['id'];
+            req.body = req.body.map((body) => {
+                const { id } = body;
                 return {
                     query: invertedMap[id],
                     ...body,
                 };
             });
             next();
+        } else if (!__DEV__ || (req.get('Referer') || '').indexOf(GRAPHIQL_ROUTE) < 0) {
+            res.status(500).send('Unknown GraphQL query has been received, rejecting...');
         } else {
-            if (!__DEV__ || (req.get('Referer') || '').indexOf(GRAPHIQL_ROUTE) < 0) {
-                res.status(500).send('Unknown GraphQL query has been received, rejecting...');
-            } else {
-                next();
-            }
+            next();
         }
     } else {
         next();
     }
 };
-
--- servers/backend-server/src/modules/module.ts
+++ servers/backend-server/src/modules/module.ts
@@ -3,37 +3,45 @@ import { Feature } from '@common-stack/server-core';
 import CounterModule from '@admin-layout/counter-module-server';
 import ScheduleModule from '@admin-layout/schedule-module-server';
 import TimeTrackerModule from '@admin-layout/timetracker-module-server';
-import { config } from '../config';
 import { TaggedType } from '@common-stack/core';
 import AuthModule from '@adminide-stack/user-auth0-server';
 import OrganizationModule from '@adminide-stack/account-api-server';
 import PlatformModule from '@adminide-stack/platform-server/lib/module';
-import MailingModule from '@adminide-stack/mailing-api-server';
+import { config } from '../config';
 
 export const settings = {
-  // mongoConnection: generateMongo(config.MONGO_URL),
-  subTopic: config.CONNECTION_ID, // usually versioning
+    // mongoConnection: generateMongo(config.MONGO_URL),
+    subTopic: config.CONNECTION_ID, // usually versioning
 };
 
-const defaultModule = () => new ContainerModule((bind: interfaces.Bind) => {
-  bind('Settings').toConstantValue(settings).whenTargetTagged('default', true);
-  bind('Settings').toConstantValue(settings).whenTargetTagged(TaggedType.MICROSERVICE, true);
-  bind('MongoOptions').toConstantValue({});
-});
+const defaultModule = () =>
+    new ContainerModule((bind: interfaces.Bind) => {
+        bind('Settings').toConstantValue(settings).whenTargetTagged('default', true);
+        bind('Settings').toConstantValue(settings).whenTargetTagged('microservice', true);
+        bind('Settings').toConstantValue(settings).whenTargetTagged(TaggedType.MICROSERVICE, true);
+        bind('MongoOptions').toConstantValue({});
+    });
 
 const DefaultFeature = new Feature({
-  createContainerFunc: [defaultModule],
-  createHemeraContainerFunc: [defaultModule],
+    createContainerFunc: [defaultModule],
+    createHemeraContainerFunc: [defaultModule],
 });
 
+let DevModules = new Feature({});
+
+if (config.isDev) {
+    const MailingModule = require('@container-stack/mailing-api-server');
+    console.log('--Mailining', MailingModule);
+    DevModules = MailingModule.default;
+}
 
 export default new Feature<any>(
     DefaultFeature,
     PlatformModule,
-    MailingModule,
     AuthModule,
     CounterModule,
     ScheduleModule,
     TimeTrackerModule,
     OrganizationModule,
+    DevModules,
 );
