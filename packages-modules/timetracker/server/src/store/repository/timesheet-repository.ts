a5582403e7c257b5c66402fac043079a3ed26c6f
gsofter <real.kingstar@outlook.com>
2021-05-05 11:02:40 -0700

    remove useless script

--- packages-modules/timetracker/server/src/store/repository/timesheet-repository.ts
+++ packages-modules/timetracker/server/src/store/repository/timesheet-repository.ts
@@ -10,109 +10,104 @@ import { ServiceBroker } from 'moleculer';
 import { TimeTrackerModelType, TimeTrackerModelFunc } from '../models/timetracker-model';
 
 export interface ITimesheetRepository {
-    getTimesheets(orgId: string, userId?: string): Promise<Array<ITimesheet>>;
-    getOrganizationTimesheets(orgId: string): Promise<Array<ITimesheet>>;
-    createTimesheet(userId: string, orgId: string, request: ITimesheetCreateRequest): Promise<boolean>;
-    updateTimesheet(orgId: string, sheetId: string, request: ITimesheetCreateRequest);
-    updateTimesheetStatus(orgId: string, sheetId: string, state: ITimesheetState);
-    removeTimesheet(userId: string, orgId: string, sheetId: string): Promise<boolean>;
+  getTimesheets(orgId: string, userId?: string): Promise<Array<ITimesheet>>;
+  getOrganizationTimesheets(orgId: string): Promise<Array<ITimesheet>>;
+  createTimesheet(userId: string, orgId: string, request: ITimesheetCreateRequest): Promise<boolean>;
+  updateTimesheet(orgId: string, sheetId: string, request: ITimesheetCreateRequest);
+  updateTimesheetStatus(orgId: string, sheetId: string, state: ITimesheetState);
+  removeTimesheet(userId: string, orgId: string, sheetId: string): Promise<boolean>;
 }
 
 @injectable()
 export class TimesheetRepository implements ITimesheetRepository {
-    private timeTrackerModel: TimeTrackerModelType;
+  private timeTrackerModel: TimeTrackerModelType;
 
-    private logger: Logger;
+  private logger: Logger;
 
-    constructor(
-        @inject('MongoDBConnection')
-        db: mongoose.Connection,
+  constructor(
+    @inject('MongoDBConnection')
+    db: mongoose.Connection,
 
-        @inject('Logger')
-        logger: Logger,
+    @inject('Logger')
+    logger: Logger,
 
-        @inject(CommonType.MOLECULER_BROKER)
-        private broker: ServiceBroker,
-    ) {
-        this.logger = logger.child({ className: 'ScheduleRepository' });
-        this.timeTrackerModel = TimeTrackerModelFunc(db);
+    @inject(CommonType.MOLECULER_BROKER)
+    private broker: ServiceBroker,
+  ) {
+    this.logger = logger.child({ className: 'ScheduleRepository' });
+    this.timeTrackerModel = TimeTrackerModelFunc(db);
+  }
+
+  public async getOrganizationTimesheets(orgId: string) {
+    const trackDoc = await this.timeTrackerModel.findOne({ orgId });
+    if (trackDoc && trackDoc.timesheets) {
+      return trackDoc.timesheets;
     }
+    return [];
+  }
 
-    public checkInPeriod(t: Date, dateA: Date, dateB: Date): boolean {
-        if (moment(dateA) < moment(dateB)) return moment(t) >= moment(dateA) && moment(t) <= moment(dateB);
-        return moment(t) >= moment(dateB) && moment(t) <= moment(dateA);
-    }
+  public async getTimesheets(orgId: string, userId?: string) {
+    const timesheets = await this.getOrganizationTimesheets(orgId);
+    return timesheets.filter((sheet) => !userId || sheet.userId === userId);
+  }
 
-    public async getOrganizationTimesheets(orgId: string) {
-        const trackDoc = await this.timeTrackerModel.findOne({ orgId });
-        if (trackDoc && trackDoc.timesheets) {
-            return trackDoc.timesheets;
-        }
-        return [];
+  public async createTimesheet(userId: string, orgId: string, request: ITimesheetCreateRequest) {
+    try {
+      const response = await this.timeTrackerModel.update(
+        { orgId },
+        { $push: { timesheets: request } },
+        { upsert: true },
+      );
+      return true;
+    } catch (err) {
+      throw new Error(err.message);
     }
+  }
 
-    public async getTimesheets(orgId: string, userId?: string) {
-        const timesheets = await this.getOrganizationTimesheets(orgId);
-        return timesheets.filter((sheet) => !userId || sheet.userId === userId);
+  public async updateTimesheet(orgId: string, sheetId: string, request: ITimesheetCreateRequest) {
+    try {
+      const response = await this.timeTrackerModel.update(
+        { orgId, timesheets: { $elemMatch: { _id: sheetId } } },
+        { $set: { 'timesheets.$': request } },
+      );
+      return response;
+    } catch (err) {
+      throw new Error(err.message);
     }
+  }
 
-    public async createTimesheet(userId: string, orgId: string, request: ITimesheetCreateRequest) {
-        try {
-            const response = await this.timeTrackerModel.update(
-                { orgId },
-                { $push: { timesheets: request } },
-                { upsert: true },
-            );
-            return true;
-        } catch (err) {
-            throw new Error(err.message);
-        }
+  public async updateTimesheetStatus(orgId: string, sheetId: string, state: ITimesheetState) {
+    try {
+      const response = await this.timeTrackerModel.update(
+        {
+          orgId,
+          $elemMatch: { 'timesheets.id': sheetId },
+        },
+        {
+          $set: {
+            'timesheets.$.status': state,
+          },
+        },
+      );
+      return response;
+    } catch (err) {
+      throw new Error(err.message);
     }
+  }
 
-    public async updateTimesheet(orgId: string, sheetId: string, request: ITimesheetCreateRequest) {
-        try {
-            const response = await this.timeTrackerModel.update(
-                { orgId, timesheets: { $elemMatch: { _id: sheetId } } },
-                { $set: { 'timesheets.$': request } },
-            );
-            return response;
-        } catch (err) {
-            throw new Error(err.message);
-        }
+  public async removeTimesheet(userId: string, orgId: string, sheetId: string) {
+    try {
+      await this.timeTrackerModel.update(
+        {
+          orgId,
+        },
+        {
+          $pull: { 'timesheets._id': sheetId },
+        },
+      );
+      return true;
+    } catch (err) {
+      throw new Error(err.message);
     }
-
-    public async updateTimesheetStatus(orgId: string, sheetId: string, state: ITimesheetState) {
-        try {
-            const response = await this.timeTrackerModel.update(
-                {
-                    orgId,
-                    $elemMatch: { 'timesheets.id': sheetId },
-                },
-                {
-                    $set: {
-                        'timesheets.$.status': state,
-                    },
-                },
-            );
-            return response;
-        } catch (err) {
-            throw new Error(err.message);
-        }
-    }
-
-    public async removeTimesheet(userId: string, orgId: string, sheetId: string) {
-        try {
-            await this.timeTrackerModel.update(
-                {
-                    orgId,
-                },
-                {
-                    $pull: { 'timesheets._id': sheetId },
-                },
-            );
-            return true;
-        } catch (err) {
-            throw new Error(err.message);
-        }
-    }
+  }
 }
