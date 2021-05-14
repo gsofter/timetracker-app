3fc9ac90dfa4e93df0a5375ddee941e8a82fc605
Veera Marni <marni.srikanth@gmail.com>
2021-05-14 18:52:56 -0400

    git remove comment

--- portable-devices/mobile/src/components/layout/module.ts
+++ portable-devices/mobile/src/components/layout/module.ts
@@ -1,30 +1,27 @@
-/* eslint-disable prettier/prettier */
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
-/* eslint-disable @typescript-eslint/no-explicit-any */
 import { Feature } from '@common-stack/client-react';
 import Layout from './Layout';
 import { SignIn, SignUp } from '../../pages';
 
 export default new Feature({
-  routeConfig: [
-    {
-      '/': {
-        exact: true,
-        component: SignIn,
-      } as any,
-    },
-    {
-      '/sign-up': {
-        exact: false,
-        component: SignUp,
-      } as any,
-    },
-    {
-      '/org': {
-        exact: false,
-        component: Layout,
-        key: 'layout',
-      } as any,
-    },
-  ],
+    routeConfig: [
+        {
+            '/': {
+                exact: true,
+                component: SignIn,
+            } as any,
+        },
+        {
+            '/sign-up': {
+                exact: false,
+                component: SignUp,
+            } as any,
+        },
+        {
+            '/org': {
+                exact: false,
+                component: Layout,
+                key: 'layout',
+            } as any,
+        },
+    ],
 });
