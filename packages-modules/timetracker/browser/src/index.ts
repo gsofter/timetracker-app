2aff31c7e64476033cdd3c6d63abef3828912947
Veera Marni <marni.srikanth@gmail.com>
2021-05-16 06:41:55 -0400

    added time reducer

--- packages-modules/timetracker/browser/src/index.ts
+++ packages-modules/timetracker/browser/src/index.ts
@@ -1,4 +1,5 @@
 import { Feature } from '@common-stack/client-react';
 import TimeTracker from './timetracker/module';
+import { timerReducer } from './redux/reducers/TimeWidgetReducer';
 
-export default new Feature(TimeTracker);
+export default new Feature(TimeTracker, { reducer: [timerReducer] as any });
