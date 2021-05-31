1974eb78600d9fa20690624efd91cc3481b59583
Veera Marni <marni.srikanth@gmail.com>
2021-05-31 08:32:08 -0400

    update

--- portable-devices/mobile/src/constants/routes.ts
+++ portable-devices/mobile/src/constants/routes.ts
@@ -1,8 +1,8 @@
 export enum MobileRoutes {
-  Home = '/',
-  login = '/login',
-  Signup = '/sign-up',
-  Logout = '/logout',
-  Welcome = '/org/welcome',
-  Org = '/org',
+    Home = '/',
+    login = '/login',
+    Signup = '/sign-up',
+    Logout = '/logout',
+    Welcome = '/orgName:/welcome',
+    Org = '/orgName:',
 }
