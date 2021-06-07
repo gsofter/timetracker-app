541c5124160d40ca1139e7c110a5a796c9994938
Muflah Nasir <muflahnasir@gmail.com>
2021-06-07 20:29:25 +0500

    Created Timesheet header

--- packages-modules/timetracker/mobile/src/timetracker/components/DayHeader.tsx
+++ packages-modules/timetracker/mobile/src/timetracker/components/DayHeader.tsx
@@ -1,61 +1,28 @@
 import React from 'react'
 import {View, Text} from "react-native"
 import {Row, Col} from 'native-base'
-import moment from 'moment'
 
 const DayHeader = ({
     styles,
-    current, 
-    total
+    time,
+    days
 }) => {
     return(
         <View style={styles.dayHeader}>
             <Text style={styles.light_grey}>TOTAL BY DAY</Text>
             <Row>
-                <Col>
-                    <Text style={styles.grey}>Mon</Text>
-                </Col>
-                <Col>
-                    <Text style={styles.grey}>Tue</Text>
-                </Col>
-                <Col>
-                    <Text style={styles.grey}>Wed</Text>
-                </Col>
-                <Col>
-                    <Text style={styles.grey}>Thu</Text>
-                </Col>
-                <Col>
-                    <Text style={styles.grey}>Fri</Text>
-                </Col>
-                <Col>
-                    <Text style={styles.grey}>Sat</Text>
-                </Col>
-                <Col>
-                    <Text style={styles.grey}>Sun</Text>
-                </Col>
+                {days.map(day => (
+                    <Col>
+                        <Text style={styles.grey}>{day}</Text>
+                    </Col>
+                ))}
             </Row>
             <Row>
-                <Col>
-                    <Text>{moment(current?.endTime).format('ddd') === 'Mon' ? total : '0:00'}</Text>
-                </Col>
-                <Col>
-                    <Text>{moment(current?.endTime).format('ddd') === 'Tue' ? total : '0:00'}</Text>
-                </Col>
-                <Col>
-                    <Text>{moment(current?.endTime).format('ddd') === 'Wed' ? total : '0:00'}</Text>
-                </Col>
-                <Col>
-                    <Text>{moment(current?.endTime).format('ddd') === 'Thu' ? total : '0:00'}</Text>
-                </Col>
-                <Col>
-                    <Text>{moment(current?.endTime).format('ddd') === 'Fri' ? total : '0:00'}</Text>
-                </Col>
-                <Col>
-                    <Text>{moment(current?.endTime).format('ddd') === 'Sat' ? total : '0:00'}</Text>
-                </Col>
-                <Col>
-                    <Text>{moment(current?.endTime).format('ddd') === 'Sun' ? total : '0:00'}</Text>
-                </Col>
+                {time.map(data => (
+                    <Col>
+                        <Text>{data}</Text>
+                    </Col>
+                ))}
             </Row>
         </View>
     )
--- packages-modules/timetracker/mobile/src/timetracker/components/TabularScreen.tsx
+++ packages-modules/timetracker/mobile/src/timetracker/components/TabularScreen.tsx
@@ -14,21 +14,44 @@ const TabularScreen = () => {
     const [totalTime, setTotalTime] = useState(moment().format('HH:mm'))
     const [title, setTitle] = useState("This Week")
     const userId = useSelector<any>((state) => state.user.auth0UserId) as string;
-    const [current, setCurrent] = useState(null)
+    const [time, setTime] = useState([])
     const [total, setTotal] = useState('')
 
     const { data, error, refetch, loading } = useGetDurationTimeRecordsQuery({
         variables: { userId: userId, startTime: weekStart, endTime: weekEnd },
     });
 
+    let days = [];
+    for (let i = 0; i <= 6; i++) {
+        days.push(moment(weekStart).add(i, 'days').format("ddd"));
+    };
+
     useEffect(() => {
-        if(data?.getDurationTimeRecords){
-            const currentTime = data.getDurationTimeRecords.find(time => moment(time.endTime).format('ddd') === moment().format('ddd'))
-            setCurrent(currentTime)
-            const totalTime = formatDuration(calcTotalTime(currentTime.startTime, currentTime.endTime), 'h:mm')
-            setTotal(totalTime)
+        if(data?.getDurationTimeRecords.length){
+            const currentTime = data.getDurationTimeRecords.filter(time => days.includes(moment(time.endTime).format('ddd')))
+            const getTime = days.map(day => {
+                return currentTime.map(time => {
+                    if(day === moment(time.endTime).format('ddd')){
+                        return(formatDuration(calcTotalTime(time.startTime, time.endTime), 'h:mm'))
+                    } else{
+                        return "0:00"
+                    }
+                })
+            })
+            const calTime = getTime.map(time => {
+                return addTime(time)
+            })
+            setTime(calTime)
+        } else{
+            const getTime = days.map(day => '0:00')
+            setTime(getTime)
         }
     }, [data?.getDurationTimeRecords])
+
+    const addTime = (times) => {
+        const sum = times.reduce((acc, time) => acc.add(moment.duration(time)), moment.duration());
+        return ([Math.floor(sum.asHours()), sum.minutes(), sum.seconds()].join(':'))
+    }
 
     const calcTotalTime = (startTime: string, endTime: string): number => {
         return Math.floor((moment(endTime).valueOf() - moment(startTime).valueOf()) / 1000);
@@ -36,7 +59,7 @@ const TabularScreen = () => {
 
     const getNextWeek = () => {
         const nextWeekStart = weekEnd.add(1, 'day')
-        const endWeekDay = moment(nextWeekStart).add(7, 'days')
+        const endWeekDay = moment(nextWeekStart).add(6, 'days')
         setWeekStart(nextWeekStart)
         setWeekEnd(endWeekDay)
         refetch()
@@ -45,7 +68,7 @@ const TabularScreen = () => {
 
     const getPastWeek = () => {
         const endWeekDay = weekStart.subtract(1, 'day')
-        const nextWeekStart = moment(endWeekDay).subtract(7, 'days')
+        const nextWeekStart = moment(endWeekDay).subtract(6, 'days')
         setWeekStart(nextWeekStart)
         setWeekEnd(endWeekDay)
         refetch()
@@ -61,7 +84,7 @@ const TabularScreen = () => {
                 <View style={styles.header}>
                     <View>
                         <Text style={styles.headerTitle}>{title}</Text>
-                        <Text style={styles.grey}>Total: {totalTime}</Text>
+                        <Text style={styles.grey}>Total: {addTime(time)}</Text>
                     </View>
                     <View style={{flexDirection: 'row'}}>
                         <TouchableHighlight onPress={() => getPastWeek()}>
@@ -73,7 +96,7 @@ const TabularScreen = () => {
                     </View>
                 </View>
                 <View style={styles.divider} />
-                <DayHeader current={current} total={total} styles={styles}/>
+                <DayHeader days={days} time={time} styles={styles}/>
             </View>
         </View>
     )
